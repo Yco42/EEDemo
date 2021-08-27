c168427711bc51c0378c5d32328f40d75ab8e6ad
testsignup125 <testsignup125@gmail.com>
2021-08-27 13:09:26 +0200

    Add project files.

---
+++ EEDemo/Helpers/scenarioRunner.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace EEDemo.Helpers.Scenarios
+{
+    
+    [TestFixture]
+    public class Scenario1
+    {
+        private string _fnameElement = "input#firstname";
+        private string _lnameElement = "input#lastname";
+        private string _price = "input#totalprice";
+        private string _deposit = "select#depositpaid";
+        private string _checkin = "input#checkin";
+        private string _checkout = "input#checkout";
+        private string _save = "input[value=' Save ']";
+
+        [Test]
+        public void RunScenario1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://hotel-test.equalexperts.io/";
+            driver.FindElement(By.CssSelector(_fnameElement)).SendKeys("!@5_ X");
+            driver.FindElement(By.CssSelector(_lnameElement)).SendKeys("ข ไข่");
+            driver.FindElement(By.CssSelector(_price)).SendKeys("10");
+            driver.FindElement(By.CssSelector(_deposit)).SendKeys("t");
+            driver.FindElement(By.CssSelector(_checkin)).SendKeys("2019-09-02");
+            driver.FindElement(By.CssSelector(_checkout)).SendKeys("2022-12-14");
+            driver.FindElement(By.CssSelector(_save)).Click();
+            driver.Dispose();
+        }
+ 
+    }
+    public class Scenario2
+    {
+        private string _fnameElement = "input#firstname";
+        private string _lnameElement = "input#lastname";
+        private string _deposit = "select#depositpaid";
+        private string _checkin = "input#checkin";
+        private string _checkout = "input#checkout";
+        private string _save = "input[value=' Save ']";
+
+        [Test]
+        public void RunScenario2()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://hotel-test.equalexperts.io/";
+            driver.FindElement(By.CssSelector(_fnameElement)).SendKeys("test");
+            driver.FindElement(By.CssSelector(_lnameElement)).SendKeys("two");
+            driver.FindElement(By.CssSelector(_deposit)).SendKeys("f");
+            driver.FindElement(By.CssSelector(_checkin)).SendKeys("20/08/2021");
+            driver.FindElement(By.CssSelector(_checkout)).SendKeys("19/08/2021");
+            driver.FindElement(By.CssSelector(_save)).Click();
+            driver.Dispose();
+        }
+
+    }
+
+    public class Scenario3
+    {
+        private string _fnameElement = "input#firstname";
+        private string _lnameElement = "input#lastname";
+        private string _price = "input#totalprice";
+        private string _deposit = "select#depositpaid";
+        private string _checkin = "input#checkin";
+        private string _checkout = "input#checkout";
+        private string _save = "input[value=' Save ']";
+
+        [Test]
+        public void RunScenario3()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://hotel-test.equalexperts.io/";
+            driver.FindElement(By.CssSelector(_fnameElement)).SendKeys("valid");
+            driver.FindElement(By.CssSelector(_lnameElement)).SendKeys("test");
+            driver.FindElement(By.CssSelector(_price)).SendKeys("10");
+            driver.FindElement(By.CssSelector(_deposit)).SendKeys("t");
+            driver.FindElement(By.CssSelector(_checkin)).SendKeys("29/08/2022");
+            driver.FindElement(By.CssSelector(_checkout)).SendKeys("30/08/2022");
+            driver.FindElement(By.CssSelector(_save)).Click();
+            driver.Dispose();
+        }
+
+    }
+}
---
+++ EEDemo/tests/websiteTests.cs
@@ -0,0 +1,36 @@
+﻿using EEDemo.Helpers.Scenarios;
+
+
+
+namespace EETestProject
+{
+    
+    public class RunTest1 : Scenario1
+        {
+            
+            private static void S1()
+            {
+                Scenario1 scenario1 = new Scenario1();
+                scenario1.RunScenario1();
+            }
+        }
+    public class RunTest2 : Scenario2
+        {
+            
+            private static void S2()
+            {
+                Scenario2 scenario2 = new Scenario2();
+                scenario2.RunScenario2();
+            }
+        }
+    public class RunTest3 : Scenario3
+    {
+        
+        private static void S3()
+        {
+            Scenario3 scenario3 = new Scenario3();
+            scenario3.RunScenario3();
+        }
+    }
+
+}
